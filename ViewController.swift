527ed5aea1d954e348214646cf4f5ec4cc18ff9b
Thomas Frey <thomas.frey@code.berlin>
2021-11-10 21:25:34 +0100

    Code running app interface

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+    //boiling times in Dictionary form
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    //Egg-Buttons Link
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        //Save button title to constant
+        //use as key value for calling cooking times
+        let hardness = sender.currentTitle!
+        
+        //Printing fitting value to key from dictionary
+        print(eggTimes[hardness]!)
+        
+        //Timer class being called (or created?) when button pressed
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    
+    var counter = 1
+    
+    //Function w/ infinite loop, being called every second.
+    //specified in Timer.scheduledTimer(...)s
+    @objc func updateCounter() {
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter += 1
+        }
+    }
+    
+}
